3a29ea8c59ec8bca5a35e8610d4c7e0c34056e7b
Anirudh Singh <anirudhrathore2003@gmail.com>
2025-08-04 17:37:46 +0530

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import "./app.css";
 
 function App() {
   return (
